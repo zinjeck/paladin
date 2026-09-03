6e9fbde503704ab73a85b280f061cbbdda5b82ce
zinjeck <asundergameco@gmail.com>
2026-09-03 18:56:31 -0400

    Update Simulation.cpp

--- src/simulation/Simulation.cpp
+++ src/simulation/Simulation.cpp
@@ -11,27 +11,87 @@ namespace Paladin
     {
     }
 
+
     Simulation::~Simulation() = default;
 
-    void Simulation::tick(double deltaSeconds)
-    {
-        world_->tick(deltaSeconds);
 
+    void Simulation::tick(
+        double realDeltaSeconds
+    )
+    {
         ++tickCount_;
+
+        const double multiplier =
+            speedMultiplier();
+
+        if (multiplier <= 0.0)
+        {
+            return;
+        }
+
+        const double gameDeltaSeconds =
+            realDeltaSeconds * multiplier;
+
+        world_->tick(gameDeltaSeconds);
     }
+
+
+    void Simulation::setSpeed(
+        SimulationSpeed speed
+    ) noexcept
+    {
+        speed_ = speed;
+    }
+
+
+    SimulationSpeed Simulation::speed() const noexcept
+    {
+        return speed_;
+    }
+
+
+    bool Simulation::isPaused() const noexcept
+    {
+        return speed_ ==
+            SimulationSpeed::Paused;
+    }
+
 
     World& Simulation::world() noexcept
     {
         return *world_;
     }
 
+
     const World& Simulation::world() const noexcept
     {
         return *world_;
     }
 
+
     std::uint64_t Simulation::tickCount() const noexcept
     {
         return tickCount_;
+    }
+
+
+    double Simulation::speedMultiplier() const noexcept
+    {
+        switch (speed_)
+        {
+            case SimulationSpeed::Paused:
+                return 0.0;
+
+            case SimulationSpeed::Normal:
+                return 1.0;
+
+            case SimulationSpeed::Fast:
+                return 2.0;
+
+            case SimulationSpeed::VeryFast:
+                return 3.0;
+        }
+
+        return 1.0;
     }
 }
