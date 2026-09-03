c54abec6ac5b42e64091f8b2e37ed66b13aa0cf5
zinjeck <asundergameco@gmail.com>
2026-09-03 18:56:14 -0400

    Update Simulation.h

--- src/simulation/Simulation.h
+++ src/simulation/Simulation.h
@@ -7,6 +7,15 @@ namespace Paladin
 {
     class World;
 
+    enum class SimulationSpeed
+    {
+        Paused,
+        Normal,
+        Fast,
+        VeryFast
+    };
+
+
     class Simulation
     {
     public:
@@ -16,15 +25,36 @@ namespace Paladin
         Simulation(const Simulation&) = delete;
         Simulation& operator=(const Simulation&) = delete;
 
-        void tick(double deltaSeconds);
+        void tick(double realDeltaSeconds);
 
+        void setSpeed(
+            SimulationSpeed speed
+        ) noexcept;
+
+        [[nodiscard]]
+        SimulationSpeed speed() const noexcept;
+
+        [[nodiscard]]
+        bool isPaused() const noexcept;
+
+        [[nodiscard]]
         World& world() noexcept;
+
+        [[nodiscard]]
         const World& world() const noexcept;
 
+        [[nodiscard]]
         std::uint64_t tickCount() const noexcept;
 
     private:
+        [[nodiscard]]
+        double speedMultiplier() const noexcept;
+
         std::unique_ptr<World> world_;
+
+        SimulationSpeed speed_ =
+            SimulationSpeed::Normal;
+
         std::uint64_t tickCount_ = 0;
     };
 }
