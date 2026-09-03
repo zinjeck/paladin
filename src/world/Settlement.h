f96516639ec4a7570a075973cfbadc3feab11ac8
zinjeck <asundergameco@gmail.com>
2026-09-03 18:20:03 -0400

    Update Settlement.h

--- src/world/Settlement.h
+++ src/world/Settlement.h
@@ -1,16 +1,21 @@
 #pragma once
 
 #include "core/StrongId.h"
+#include "world/WorldPosition.h"
 
 namespace Paladin
 {
+    class World;
+
     class Settlement
     {
     public:
-        explicit Settlement(
-            SettlementId id
+        Settlement(
+            SettlementId id,
+            WorldPosition position
         ) noexcept
-            : id_(id)
+            : id_(id),
+              position_(position)
         {
         }
 
@@ -20,7 +25,46 @@ namespace Paladin
             return id_;
         }
 
+        [[nodiscard]]
+        WorldPosition position() const noexcept
+        {
+            return position_;
+        }
+
+        [[nodiscard]]
+        PolityId ownerPolityId() const noexcept
+        {
+            return ownerPolityId_;
+        }
+
+        [[nodiscard]]
+        bool hasOwnerPolity() const noexcept
+        {
+            return ownerPolityId_.isValid();
+        }
+
     private:
+        friend class World;
+
+        void setPosition(
+            WorldPosition position
+        ) noexcept
+        {
+            position_ = position;
+        }
+
+        void setOwnerPolity(
+            PolityId polityId
+        ) noexcept
+        {
+            ownerPolityId_ = polityId;
+        }
+
         SettlementId id_;
+
+        WorldPosition position_;
+
+        // Invalid ID means independent / currently unowned.
+        PolityId ownerPolityId_;
     };
 }
