5b4991043990328366cac3c0d2ba1f0e3cabc71c
zinjeck <asundergameco@gmail.com>
2026-09-10 06:19:23 -0400

    Let world pixel scenes crossfade and rotate as a composed layer

--- src/rendering/WorldPixelGrid.h
+++ src/rendering/WorldPixelGrid.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "rendering/Renderer.h"
 #include <algorithm>
+#include <cstdint>
 namespace Paladin
 {
     // One art pixel is 1/16 of a world tile, including fitted buildings,
@@ -14,18 +15,34 @@ namespace Paladin
     {
         Renderer& renderer_;
         bool active_;
+        std::uint8_t opacity_ = 255;
+        double rotationDegrees_ = 0.0;
+        double compositeScale_ = 1.0;
 
     public:
-        WorldPixelScene(Renderer& r, double tilePixels)
+        WorldPixelScene(
+            Renderer& r,
+            double tilePixels,
+            std::uint8_t opacity = 255,
+            double rotationDegrees = 0.0,
+            double compositeScale = 1.0
+        )
             : renderer_(r),
-              active_(r.beginPixelScene(worldPixelPitch(tilePixels)))
+              active_(r.beginPixelScene(worldPixelPitch(tilePixels))),
+              opacity_(opacity),
+              rotationDegrees_(rotationDegrees),
+              compositeScale_(compositeScale)
         {
         }
         ~WorldPixelScene()
         {
             if (active_)
             {
-                renderer_.endPixelScene();
+                renderer_.endPixelScene(
+                    opacity_,
+                    rotationDegrees_,
+                    compositeScale_
+                );
             }
         }
         WorldPixelScene(const WorldPixelScene&) = delete;
